981fa74e2a5e29ae4ee30bf0bc24eb1ed0eb41cd
Konstantin <telyakovkostya.learn@gmail.com>
2022-06-22 02:39:48 +0300

    fix readme

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,117 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="example">
+    <div class="example__inputs">
+      <input 
+        v-if="state.value.not_exist.first.visible" 
+        type="text" 
+        placeholder="first" 
+        :disabled="false"
+      >
+      <input 
+        v-if="state.value.not_exist.second.visible" 
+        type="text" 
+        placeholder="second" 
+        :disabled="!!state.value.not_exist.second.visible.disabled"
+      >
+      <input 
+        v-if="state.value.not_exist.third.visible" 
+        type="text" 
+        placeholder="third"
+        :disabled="state.value.not_exist.third.visible === 'disabled'"
+      >
+    </div>
+    <div class="example__state">
+      {{ state.value }}
+    </div>
+    <div class="example__buttons">
+      <button @click="send('FIRST_STATE')">FIRST_STATE</button>
+      <button @click="send('SECOND_STATE')">SECOND_STATE</button>
+      <button @click="send('DEFAULT')">DEFAULT STATE</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { useMachine } from 'xstate-vue2';
+import { createMachine } from 'xstate';
+
+function createInputState(id) {
+  return {
+    [id]: {
+      id,
+      initial: "visible",
+      states: {
+        // Состояния инпута, когда он видимый
+        visible: {
+          initial: 'default',
+          states: {
+            default: { type: 'final' },
+            disabled: { type: 'final' },
+            required: { type: 'final' },
+          }
+        },
+        // Инпут скрыт
+        hidden: {},
+      },
+      on: {
+        [`${id}.DISABLED`]: { target: ".visible.disabled" },
+        [`${id}.REQUIRED`]: { target: ".visible.required" },
+        [`${id}.DEFAULT`]: { target: ".visible.default" },
+        [`${id}.HIDDEN`]: { target: ".hidden" }
+      },
+    }
+  };
+}
+
+const toggleMachine = createMachine({
+  id: "testMachine",
+  initial: "not_exist",
+  states: {
+    not_exist: {
+      type: "parallel",
+      states: {
+        ...createInputState("first"),
+        ...createInputState("second"),
+        ...createInputState("third"),
+        home_in_schedule: {
+          type: "atomic",
+          on: {
+            DEFAULT: { target: [
+              "first.visible.default",
+              "second.visible.default",
+              "third.visible.default"
+            ]},
+            FIRST_STATE: { target: [
+              "first.hidden",
+              "second.visible.disabled",
+              "third.hidden"
+            ]},
+            SECOND_STATE: { target: [
+              "first.visible.disabled",
+              "second.hidden",
+              "third.visible.disabled"
+            ]},
+          },
+        },
+      },
+    },
+  },
+})
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const { state, send } = useMachine(toggleMachine);
+    return {
+      state,
+      send
+    };
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.example {
+  display: flex;
+  flex-direction: column;
 }
 </style>
